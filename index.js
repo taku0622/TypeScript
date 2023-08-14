394b54cdc60c87cf8192d5af9bb58084a9f64b04
taku0622 <hayatsutkm@gmail.com>
2023-08-14 13:21:56 +0900

    done

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 var hello = 'hello';
 console.log(hello);
+// boolean
+var hasValue = true;
+// number
+var count = 10;
+var float = 3.14;
+var negative = -0.12;
--- index.ts
+++ index.ts
@@ -1,2 +1,15 @@
 let hello: string = 'hello';
 console.log(hello);
+
+// boolean
+let hasValue: boolean = true;
+
+// number
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12;
+
+// string
+let single: string = 'hello';
+let double: string = "hello";
+let back: string = `hello`;
